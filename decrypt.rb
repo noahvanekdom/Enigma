8d532e88006bd399dd1f12ef4fb8f246fa7ecee1
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-09 13:33:57 -0600

    Add working version of decrypt runner

--- decrypt.rb
+++ decrypt.rb
@@ -1,1 +1,14 @@
 #decrypt runner
+require_relative './lib/enigma'
+
+enigma = Enigma.new
+
+encrypted_message = File.open(ARGV[0]).read
+
+decrypted_hash = enigma.decrypt(encrypted_message, ARGV[2], ARGV[3])
+
+decrypted_message = File.open(ARGV[1], "w")
+
+decrypted_message.write(decrypted_hash[:decryption])
+
+p "Created '#{ARGV[1]}' with the key #{decrypted_hash[:key]} and date #{decrypted_hash[:date]}"
