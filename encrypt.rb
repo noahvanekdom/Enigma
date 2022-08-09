5183920df576b707c86ee563b04b778abb67cd06
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-09 13:11:32 -0600

    Add encrypt enigma runner, file opening functional

--- encrypt.rb
+++ encrypt.rb
@@ -3,4 +3,13 @@ require_relative './lib/enigma'
 
 enigma = Enigma.new
 
-read_file = File.open(ARGV[0], 'r')
+original_message = File.open(ARGV[0]).read
+
+encrypted_hash = enigma.encrypt(original_message)
+
+decryption = decryption_hash[:decryption]
+
+
+# encryption = enigma.encrypt[:encryption]
+# key = enigma.encrypt[:key]
+# date = enigma.encrypt[:date]
