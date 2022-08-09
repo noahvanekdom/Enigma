8b30dcee61ea9cc5107197671162ca2ebfa02005
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-09 06:23:46 -0600

    Add outline of tests for encryption

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,11 +3,15 @@ require_relative 'spec_helper'
 describe Enigma do
     let(:enigma) { Enigma.new }
     describe 'initialize' do
-        it 'exists' do
+        xit 'exists' do
             expect(enigma).to be_instance_of Enigma
         end
 
-        it 'can encrypt a string' do
+        describe 'date_string creation' do
+            has
+            it
+
+        xit 'can encrypt a string' do
             expected = {
                 encryption: "keder ohulw",
                 key: "02715",
@@ -20,31 +24,47 @@ describe Enigma do
     end
 
     describe 'creating keys' do
-        it 'can create the a key' do
+        xit 'can create the a key' do
             expect(enigma.a_key('02715')).to eq '02'
         end
 
-        it 'can create the B key' do
+        xit 'can create the B key' do
             expect(enigma.b_key('02715')).to eq '27'
         end
 
-        it 'can create the C key' do
+        xit 'can create the C key' do
             expect(enigma.c_key('02715')).to eq '71'
         end
 
-        it 'can create the D key' do
+        xit 'can create the D key' do
             expect(enigma.d_key('02715')).to eq '15'
         end
     end
 
     describe 'creating offsets' do
-        it 'can create the A offset' do
+        xit 'can create the A offset' do
         end
-        it 'can create the A offset' do
+        xit 'can create the A offset' do
         end
-        it 'can create the A offset' do
+        xit 'can create the A offset' do
         end
-        it 'can create the A offset' do
+        xit 'can create the A offset' do
         end
     end
+
+    describe 'creating shift' do
+        xit 'can create the A shift' do
+        end
+
+        xit 'can create the B shift' do
+        end
+
+        xit 'can create the C shift' do
+        end
+
+        it 'can create the D shift' do
+        end
+    end
+
+
 end
