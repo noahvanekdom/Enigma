65dff8fe68761280bb54727a66763c4eae27e800
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-09 10:37:49 -0600

    Add almost working encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,12 +8,6 @@ class Enigma
         @rand_key = (Array.new(5) { rand(0..9) }).join
     end
 
-        #seems like we dont need this
-
-    # def offset_generator(date)
-    #    ((date.to_i) ** 2).to_s[-4..-1]
-    # end
-
     def shift_generator(key, date)
         a_shift = (key[0..1].to_i + (date.to_i ** 2).to_s[-4..-1][0].to_i)
         b_shift = (key[1..2].to_i + (date.to_i ** 2).to_s[-4..-1][1].to_i)
@@ -29,22 +23,9 @@ class Enigma
             if @character_array.include?(character) == false
                 character
             else
-                @character_array[(@character_array.index(character) + shifts[index % 4]) % 27]
+                @character_array[(@character_array.index(character) + shifts[index])]
             end
         end.join
         {encryption: encryption, key: key, date: date}
-    end
-
-    def decrypt(message, key = @rand_key, date = @date)
-        message = message.downcase.chars
-        shifts = shift_generator(key, date)
-        decryption = message.map.with_index do |character, index|
-            if @character_array.include?(character) == false
-                character
-            else
-                @character_array[(@character_array.index(character) - shifts[index % 4] % 27)]
-            end
-        end.join
-        {decryption: decryption, key: key, date: date}
     end
 end
