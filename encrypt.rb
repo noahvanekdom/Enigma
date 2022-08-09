9b2d4a04c6d4462482b4e2c90560ca9ae9d91fb6
Noah van Ekdom <noahvanekdom@gmail.com>
2022-08-09 13:32:55 -0600

    Add working version of encrypt runner

--- encrypt.rb
+++ encrypt.rb
@@ -7,8 +7,9 @@ original_message = File.open(ARGV[0]).read
 
 encrypted_hash = enigma.encrypt(original_message)
 
-decryption = decryption_hash[:decryption]
+File.open(ARGV[1], "w").write(encrypted_hash[:encryption])
 
+p "Created '#{ARGV[1]}' with the key #{encrypted_hash[:key]} and date #{encrypted_hash[:date]}"
 
 # encryption = enigma.encrypt[:encryption]
 # key = enigma.encrypt[:key]
